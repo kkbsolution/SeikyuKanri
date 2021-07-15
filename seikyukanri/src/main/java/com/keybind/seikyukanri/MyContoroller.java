d0efe4dfea360ce1c42975f6bf89520330a930bf
t-imada <t-imada@keybind.com>
2021-07-15 16:02:08 +0900

    今田　更新
    コントローラーのコメントを追加しました

--- seikyukanri/src/main/java/com/keybind/seikyukanri/MyContoroller.java
+++ seikyukanri/src/main/java/com/keybind/seikyukanri/MyContoroller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
+//初めてのgithub
 @Controller
 public class MyContoroller {
 	//接続時にログイン画面を表示させる
