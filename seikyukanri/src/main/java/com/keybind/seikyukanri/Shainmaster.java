48217dac73cacf249289f0e40a3fd0274fbf0103
m-sonoda <m-sonoda@keybind.com>
2021-07-27 14:16:09 +0900

    園田　更新
    Entityとrepositoryの追加

---
+++ seikyukanri/src/main/java/com/keybind/seikyukanri/Shainmaster.java
@@ -0,0 +1,94 @@
+package com.keybind.seikyukanri;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Shainmaster")
+public class Shainmaster {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String shain_no;
+	
+	@Column
+	private String shain_name;
+	@Column
+	private String division;
+	@Column
+	private String post;
+	@Column
+	private String mailaddress;
+	@Column
+	private String password;
+	@Column
+	private String permission;
+	@Column
+	private Date applying_date;
+	@Column
+	private String last_division;
+	
+	
+	public String getShain_no() {
+		return shain_no;
+	}
+	public void setShain_no(String shain_no) {
+		this.shain_no = shain_no;
+	}
+	public String getShain_name() {
+		return shain_name;
+	}
+	public void setShain_name(String shain_name) {
+		this.shain_name = shain_name;
+	}
+	public String getDivision() {
+		return division;
+	}
+	public void setDivision(String division) {
+		this.division = division;
+	}
+	public String getPost() {
+		return post;
+	}
+	public void setPost(String post) {
+		this.post = post;
+	}
+	public String getMailaddress() {
+		return mailaddress;
+	}
+	public void setMailaddress(String mailaddress) {
+		this.mailaddress = mailaddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPermission() {
+		return permission;
+	}
+	public void setPermission(String permission) {
+		this.permission = permission;
+	}
+	public Date getApplying_date() {
+		return applying_date;
+	}
+	public void setApplying_date(Date applying_date) {
+		this.applying_date = applying_date;
+	}
+	public String getLast_division() {
+		return last_division;
+	}
+	public void setLast_division(String last_division) {
+		this.last_division = last_division;
+	}
+	
+	
+}
---
+++ seikyukanri/src/main/java/com/keybind/seikyukanri/Shainmasterrepository.java
@@ -0,0 +1,7 @@
+package com.keybind.seikyukanri;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Shainmasterrepository extends JpaRepository<Shainmaster, String> {
+
+}
